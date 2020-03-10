a4fa92321303b7360f90fe743f8276520425f9c1
TurnTableTyler <51184665+TurnTableTyler@users.noreply.github.com>
2020-03-09 23:59:11 -0400

    Update PhoneDetailActivity.java

--- app/src/main/java/com/google/firebase/example/flipphone/PhoneDetailActivity.java
+++ app/src/main/java/com/google/firebase/example/flipphone/PhoneDetailActivity.java
@@ -207,7 +207,8 @@ public class PhoneDetailActivity extends AppCompatActivity implements
     }
 
     public void onAddRatingClicked(View view) {
-        mRatingDialog.show(getSupportFragmentManager(), RatingDialogFragment.TAG);
+        Intent chatIntent = new Intent(getApplicationContext(), Chat.class);
+        startActivity(chatIntent);
     }
 
     @Override
