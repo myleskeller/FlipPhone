9197b45ba31f48a54eb2c91bb0dcf5125b818fdc
TurnTableTyler <51184665+TurnTableTyler@users.noreply.github.com>
2020-03-09 20:52:20 -0400

    Add files via upload

--- app/src/main/java/com/google/firebase/example/flipphone/Chat.java
+++ app/src/main/java/com/google/firebase/example/flipphone/Chat.java
@@ -1,14 +1,14 @@
 package com.google.firebase.example.flipphone;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class Chat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_messaging);
     }
 }
