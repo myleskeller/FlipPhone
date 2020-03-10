640619521ed14da0ad84f8ab6fe0a692a30ff901
TurnTableTyler <51184665+TurnTableTyler@users.noreply.github.com>
2020-03-10 01:11:00 -0400

    Add files via upload

---
+++ app/src/main/java/com/google/firebase/example/flipphone/SellActivity.java
@@ -0,0 +1,14 @@
+package com.google.firebase.example.flipphone;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SellActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_device);
+    }
+}
