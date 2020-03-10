6c5c3aaa7f0fa87ff260fca29dd98a1e0b5d4a2c
TurnTableTyler <51184665+TurnTableTyler@users.noreply.github.com>
2020-03-09 23:04:16 -0400

    Added Sell_success

--- app/src/main/java/com/google/firebase/example/flipphone/SellFlip.java
+++ app/src/main/java/com/google/firebase/example/flipphone/SellFlip.java
@@ -2,7 +2,10 @@ package com.google.firebase.example.flipphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SellFlip extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SellFlip extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_flip);
+
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent SellIntent = new Intent(getApplicationContext(), SellSuccess.class);
+                startActivity(SellIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/google/firebase/example/flipphone/SellSuccess.java
@@ -0,0 +1,14 @@
+package com.google.firebase.example.flipphone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_success);
+    }
+}
