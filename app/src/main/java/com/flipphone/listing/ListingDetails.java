a10aad8604cdad9c396230a0c08dfd284210e0d8
robsi <robsimon01@gmail.com>
2020-03-29 17:59:50 -0400

    Added phone brand and model drop down menu when posting listing.

--- app/src/main/java/com/flipphone/listing/ListingDetails.java
+++ app/src/main/java/com/flipphone/listing/ListingDetails.java
@@ -5,8 +5,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,6 +30,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.example.flipphone.MainActivity;
 import com.google.firebase.example.flipphone.R;
 import com.google.firebase.example.flipphone.model.Phone;
+import com.google.firebase.example.flipphone.util.PhoneUtil;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -39,17 +44,24 @@ import com.google.firebase.storage.StorageMetadata;
 import com.google.firebase.storage.StorageReference;
 import com.google.gson.internal.$Gson$Preconditions;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListingDetails extends AppCompatActivity implements
         View.OnClickListener{
     private FirebaseFirestore mFirestore;
     private CollectionReference mPhoneRef;
+    private Spinner phoneBrand;
+    private Spinner phoneModel;
     private int price;
     EditText description;
     private String condition;
     String photoFront;
     String photoBack;
+    String brand;
+    String model;
+
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
@@ -59,6 +71,87 @@ public class ListingDetails extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listing_details_activity);
+        String[] temp = {"-Select Model-"};
+        phoneBrand = findViewById(R.id.brand_textView);
+        phoneModel = findViewById(R.id.model_textView);
+        ArrayAdapter<String> startingValue = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, temp);
+        phoneModel.setAdapter(startingValue);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, PhoneUtil.BRAND);
+        phoneBrand.setAdapter(adapter1);
+        phoneBrand.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                brand = phoneBrand.getSelectedItem().toString();
+               switch (brand) {
+                   case "Apple":
+                       ArrayAdapter<String> adapter2 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Apple);
+                       phoneModel.setAdapter(adapter2);
+                       break;
+                   case "Samsung":
+                       ArrayAdapter<String> adapter3 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Samsung);
+                       phoneModel.setAdapter(adapter3);
+                       break;
+                   case "Motorola":
+                       ArrayAdapter<String> adapter4 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Motorola);
+                       phoneModel.setAdapter(adapter4);
+                       break;
+                   case "Huawei":
+                       ArrayAdapter<String> adapter5 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Huawei);
+                       phoneModel.setAdapter(adapter5);
+                       break;
+                   case "HTC":
+                       ArrayAdapter<String> adapter6 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.HTC);
+                       phoneModel.setAdapter(adapter6);
+                       break;
+                   case "Razer":
+                       ArrayAdapter<String> adapter7 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Razer);
+                       phoneModel.setAdapter(adapter7);
+                       break;
+                   case "OnePlus":
+                       ArrayAdapter<String> adapter8 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.OnePlus);
+                       phoneModel.setAdapter(adapter8);
+                       break;
+                   case "Google":
+                       ArrayAdapter<String> adapter9 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Google);
+                       phoneModel.setAdapter(adapter9);
+                       break;
+                   case "LG":
+                       ArrayAdapter<String> adapter10 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.LG);
+                       phoneModel.setAdapter(adapter10);
+                       break;
+                   case "Sony":
+                       ArrayAdapter<String> adapter11 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Sony);
+                       phoneModel.setAdapter(adapter11);
+                       break;
+                   case "Xiaomi":
+                       ArrayAdapter<String> adapter12 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.Xiaomi);
+                       phoneModel.setAdapter(adapter12);
+                       break;
+                   case "Oppo":
+                       ArrayAdapter<String> adapter13 = new ArrayAdapter<>(ListingDetails.this, android.R.layout.simple_list_item_1, PhoneUtil.OPPO);
+                       phoneModel.setAdapter(adapter13);
+                       break;
+               }
+               phoneModel.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                   @Override
+                   public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                       model = phoneModel.getSelectedItem().toString();
+                   }
+
+                   @Override
+                   public void onNothingSelected(AdapterView<?> parent) {
+
+                   }
+               });
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         TextView priceTextView = findViewById(R.id.price_value);
         description = findViewById(R.id.description_text);
         SeekBar conditionBar = findViewById(R.id.condition_seekBar);
@@ -140,7 +233,7 @@ public class ListingDetails extends AppCompatActivity implements
                 phone.setPrice(price);
                 phone.setCity(condition);
                 phone.setUserid(user);
-                phone.setName("Pen");
+                phone.setName(brand + " " + model);
                 FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
                 StorageReference storageReference =  firebaseStorage.getReference().child("users").child(user);
                 phone.setPhoto(photoFront);
--- app/src/main/java/com/google/firebase/example/flipphone/util/PhoneUtil.java
+++ app/src/main/java/com/google/firebase/example/flipphone/util/PhoneUtil.java
@@ -48,7 +48,8 @@ public class PhoneUtil {
 
     private static final int MAX_IMAGE_NUM = 5;
 
-    private static final String[] NAME_FIRST_WORDS = {
+    public static String[] BRAND = {
+            "-Select Brand-",
             "Samsung",
             "Apple",
             "Motorola",
@@ -57,19 +58,241 @@ public class PhoneUtil {
             "Razer",
             "OnePlus",
             "Google",
-            "Best",
+            "LG",
+            "Sony",
+            "Xiaomi",
+            "Oppo"
     };
-
-    private static final String[] NAME_SECOND_WORDS = {
-            "Phone",
-            "One",
-            "iPhone Pro Max",
+    public static String[] Apple = {
+            "-Select Model-",
+            "iPhone 6",
+            "iPhone 6 Plus",
+            "iPhone 6s",
+            "iPhone 6s Plus",
+            "iPhone SE",
+            "iPhone 7",
+            "iPhone 7 Plus",
+            "iPhone 8",
+            "iPhone 8 Plus",
+            "iPhone X",
+            "iPhone XR",
+            "iPhone XS",
+            "iPhone XS Max",
+            "iPhone 11",
+            "iPhone 11 Pro",
+            "iPhone 11 Pro Max"
+    };
+     public static String[] Samsung = {
+             "-Select Model-",
+            "Galaxy S7",
+            "Galaxy S7 Edge",
+            "Galaxy S8",
+            "Galaxy S8+",
+            "Galaxy S8 Active",
+            "Galaxy S9",
+            "Galaxy S9+",
+            "Galaxy S9 Light Luxury",
+            "Galaxy S10",
+            "Galaxy S10+",
+            "Galaxy S10e",
+            "Galaxy S10 5G",
             "Galaxy S20",
-            "8",
-            "Moto G8",
-            "Pixel",
-    };
+            "Galaxy S20 Ultra",
+            "Galaxy S20 Ultra 5G",
+            "Galaxy S20+ 5G",
+            "Galaxy S20 5G",
+            "Galaxy Fold",
+            "Galaxy Fold 5G",
+             "Galaxy Note 8",
+             "Galaxy Note 9",
+             "Galaxy Note 10",
+             "Galaxy Note 10+",
+             "Galaxy A01",
+             "Galaxy A11",
+             "Galaxy A31",
+             "Galaxy A41",
+             "Galaxy A51",
+             "Galaxy A71",
+             "Galaxy A3",
+             "Galaxy A5",
+             "Galaxy A6",
+             "Galaxy A6+",
+             "Galaxy A6s",
+             "Galaxy A7",
+             "Galaxy A8",
+             "Galaxy A8+",
+             "Galaxy A8s",
+             "Galaxy A8 Star",
+             "Galaxy A9",
+             "Galaxy A10e",
+             "Galaxy A10s",
+             "Galaxy A20",
+             "Galaxy A30",
+             "Galaxy A40",
+             "Galaxy A50",
+             "Galaxy A60",
+             "Galaxy A70",
+             "Galaxy A80"
 
+    };
+     public static String [] Motorola = {
+             "-Select Model-",
+             "Moto Z",
+             "Moto Z Play",
+             "Moto Z2 Play",
+             "Moto Z3",
+             "Moto Z3 Play",
+             "Moto Z4",
+             "Moto X4",
+             "Motorola Razr",
+             "Motorola One",
+             "Motorola One Power",
+             "Motorola One Vision",
+             "Motorola One Action",
+             "Motorola One Zoom",
+             "Motorola One Macro",
+             "Motorola One Hyper",
+             "Moto E4",
+             "Moto E4 Play",
+             "Moto E5",
+             "Moto E5 Play",
+             "Moto E5 Plus",
+             "Moto E6",
+             "Moto E6s",
+             "Moto E6 Play",
+             "Moto E6 Plus",
+             "Moto G5",
+             "Moto G5 Plus",
+             "Moto G6",
+             "Moto G6 Play",
+             "Moto G6 Plus",
+             "Moto G7",
+             "Moto G7 Play",
+             "Moto G7 Plus",
+             "Moto G8",
+             "Moto G8 Play",
+             "Moto G8 Plus"
+     };
+    public static String[] Huawei ={
+            "-Select Model-",
+            "Mate 10",
+            "Mate 10 Pro",
+            "Mate 10 Lite",
+            "Porsche Design Mate 10",
+            "Porsche Design Mate RS",
+            "Mate 20",
+            "Mate 20 Pro",
+            "Mate 20 Lite",
+            "Mate 20 X",
+            "Mate 20 Porsche RS",
+            "Mate X",
+            "Mate 30",
+            "Mate 30 Pro",
+            "Mate 30",
+            "Mate 30 Pro",
+            "Mate 30 RS",
+            "Mate Xs"
+    };
+    public static String[] HTC ={
+            "-Select Model-",
+            "U11",
+            "U11+",
+            "U12",
+            "U12+",
+            "U12 Life",
+            "Exodus",
+            "U19e",
+            "Desire 19+",
+            "Wildfire X",
+            "Desire 12",
+            "Desire 12+"
+    };
+    public static String[] Razer = {
+            "Phone",
+            "Phone 2"
+    };
+    public static String[] OnePlus ={
+            "-Select Model-",
+        "OnePlus 5",
+            "OnePlus 5T",
+            "OnePlus 6",
+            "OnePlus 6T",
+            "OnePlus 7",
+            "OnePlus 7 Pro",
+            "OnePlus 7T",
+            "OnePlus 7T Pro"
+    };
+    public static String[] Google = {
+            "-Select Model-",
+            "Pixel 3",
+            "Pixel 3 XL",
+            "Pixel 3a",
+            "Pixel 3a XL",
+            "Pixel 4",
+            "Pixel 4 XL"
+    };
+    public static String[] LG = {
+            "-Select Model-",
+            "V50 ThinQ",
+            "V40 ThinQ",
+            "V35 ThinQ",
+            "V30",
+            "G8 ThinQ",
+            "G7 ThinQ",
+            "G6"
+    };
+    public static String[] Sony = {
+            "-Select Model-",
+        "Xperia L2",
+            "Xperia XA2",
+            "Xperia XA2 Ultra",
+            "Xperia XA2 Plus",
+            "Xperia XZ2",
+            "Xperia XZ2 Compact",
+            "Xperia XZ2 Premium",
+            "Xperia XZ3",
+            "Xperia L3",
+            "Xperia 10",
+            "Xperia 10 Plus",
+            "Xperia 1",
+            "Xperia Ace",
+            "Xperia 5",
+            "Xperia 8",
+            "Xperia L4",
+            "Xperia 1 II",
+            "Xperia 10 II",
+            "Xperia Pro"
+
+
+
+    };
+    public static String[] Xiaomi = {
+            "-Select Model-",
+                "Mi 9",
+                "Mi 9T",
+                "9T Pro",
+                "Mi Note 10",
+                "Mi Mix 3",
+                "Redmi Note 7 series",
+                "Mi A3",
+                "Redmi Note 8T",
+                "Redmi Note 8 Pro",
+                "Redmi 7",
+                "Redmi 8A",
+                "Redmi 8"
+    };
+    public static String[] OPPO = {
+            "-Select Model-",
+                    "Reno 10x Zoom",
+                    "Reno 2",
+                    "Reno 2Z",
+                    "Reno Z",
+                    "Reno",
+                    "A9 2020",
+                    "RX17 Pro",
+                    "Find X",
+                    "RX17 Neo"
+    };
 
     /**
      * Create a random Phone POJO.
@@ -151,9 +374,9 @@ public class PhoneUtil {
         return min + (random.nextDouble() * 4.0);
     }
 
-    private static String getRandomName(Random random) {
-        return getRandomString(NAME_FIRST_WORDS, random) + " "
-                + getRandomString(NAME_SECOND_WORDS, random);
+    private  String getRandomName(Random random) {
+        return getRandomString(BRAND, random) + " "
+                + getRandomString(Samsung, random);
     }
 
     private static String getRandomString(String[] array, Random random) {
