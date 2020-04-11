f96047665d8a1a998b17a702ee3b7fc52bab01fa
Rob <robsimon01@gmail.com>
2020-04-11 18:21:41 -0400

    deleted some stuff

--- app/src/main/java/com/flipphone/adapter/FirestoreAdapter.java
+++ app/src/main/java/com/flipphone/adapter/FirestoreAdapter.java
@@ -1,18 +1,4 @@
-/**
- * Copyright 2017 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
  package com.flipphone.adapter;
 
 import android.util.Log;
@@ -29,16 +15,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-/**
- * RecyclerView adapter for displaying the results of a Firestore {@link Query}.
- *
- * Note that this class forgoes some efficiency to gain simplicity. For example, the result of
- * {@link DocumentSnapshot#toObject(Class)} is not cached so the same object may be deserialized
- * many times as the user scrolls.
- * 
- * See the adapter classes in FirebaseUI (https://github.com/firebase/FirebaseUI-Android/tree/master/firestore) for a
- * more efficient implementation of a Firestore RecyclerView Adapter.
- */
+
 public abstract class FirestoreAdapter<VH extends RecyclerView.ViewHolder>
         extends RecyclerView.Adapter<VH>
 implements EventListener<QuerySnapshot>{
